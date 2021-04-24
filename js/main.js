0c165797a2a665574c33e6f36b56bc7068f1cdab
Yasaman <y.k.r7713@gmail.com>
2021-04-25 00:57:17 +0430

    0:58

--- js/main.js
+++ js/main.js
@@ -279,3 +279,34 @@ $('.show-cart').on("change", ".item-count", function(event) {
 
 displayCart();
 
+// $(document).ready(function(){
+
+//   $("#edit-inf").click(function(){
+//     $("#show-inf").show();
+//   });
+// });
+
+// $(document).ready(function(){
+
+
+//   $("#edit-pass").click(function(){
+//     $("#show-pass").show();
+
+
+//     // $('#show-pass').css('display','block');
+//   });
+// });
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
